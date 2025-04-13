0b0e6ea9496ca76d722598ca685dc62d1db2eb3a
jchang118 <josephchang911@msn.com>
2025-04-14 08:27:53 +1000

    13 April done

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,5 @@
+let country = "China";
+let continent = "Asia";
+let population = 1400;
+
+console.log(country, "is in", continent, "and has", population/1000, "billion population.");
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,105 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+//
+// console.log("Jonas");
+// console.log(23);
+//
+// let firstName = "Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+//
+// let jonas_matilda = "JM";
+// let $function = 27;
+// let person = "jonas";
+// let PI = 3.1415;
+//
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+//
+// let job1 = "programmer";
+// let job2 = "teacher";
+//
+// console.log(myFirstJob);
+//
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+//
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+//
+// javascriptIsFun = "Yes";
+// console.log(typeof javascriptIsFun);
+//
+// let year;
+// console.log(year);
+// console.log(typeof year);
+//
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
+//
+// console.log(typeof null);
+//
+// let age = 30;
+// age = 31;
+//
+// const birthYear = 1991;
+// birthYear = 1990; -- > not allowed
+//
+// const job; --> not allowed, has to be an initial value
+//
+// var job = "programmer"; --> always use LET instead of VAR!!!
+// job = "teacher";
+//
+// lastName = "Chang"; --> without LET OR CONST, this would be treated as a global variable
+// console.log(lastName);
+//
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Joseph";
+const lastName = "Chang";
+console.log(firstName, lastName + " is brilliant!");
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+console.log(now - 1991 > now - 2018);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
