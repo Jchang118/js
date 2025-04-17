b9eca7f63a16379ba56854f865f12df5bbd9bfaa
jchang118 <josephchang911@msn.com>
2025-04-17 19:54:15 +0700

    function intro

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,47 @@
+// 'use strict';
+//
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio'; --> reserved word
+// const private = 'private'; --> reserved word
+
+// function logger() {
+//     console.log("My name is Joseph");
+// }
+//
+// logger();
+// logger();
+// logger();
+//
+// function fruitProcessor(apples, oranges) {
+//     return `Juice with ${apples} apples and ${oranges} oranges`;
+// }
+//
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+//
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+//
+// const num = Number('23');
+// console.log(num);
+
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+//
+// const age1 = calcAge1(1991);
+// console.log(age1);
+//
+// const calcAge2 = function(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+// console.log(age2);
+//
+// console.log(age1, age2);
