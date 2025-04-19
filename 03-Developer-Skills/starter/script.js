9b964387d446968e1bfca2682aa0ac558177457e
jchang118 <josephchang911@msn.com>
2025-04-19 18:11:38 +0800

    19th April part 1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-console.log("abc");
+function printForecast(arr) {
+  let res = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      res += `... ${arr[i]}°C in ${i + 1} day`;
+    } else {
+      res += `... ${arr[i]}°C in ${i + 1} days`;
+    }
+    if (i < arr.length - 1) {
+      res += " ";
+    }
+  }
+  console.log(res);
+}
 
-console.log("uts");
+let arr1 = [17, 21, 23];
+let arr2 = [12, 5, -5, 0, 4];
+
+printForecast(arr1);
+printForecast(arr2);
